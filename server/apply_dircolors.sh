83919cf62bf32a60bfa236f9e1b09e10f48074f4
Kyo Yamada <hysteric.madam@gmail.com>
2020-01-03 18:09:25 +0900

    revise: change theme to seebi/dircolors-solarized

--- server/apply_dircolors.sh
+++ server/apply_dircolors.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
-echo "Start 'git clone' Nord dircolors"
-echo "***[https://github.com/arcticicestudio/nord-dircolors]"
+echo "Start 'git clone' Solarized Color Theme for GNU"
+echo "***[https://github.com/seebi/dircolors-solarized]"
 
 mkdir -p ~/tmp
-git clone git@github.com:arcticicestudio/nord-dircolors.git ~/tmp/.
+git clone git@github.com:seebi/dircolors-solarized.git ~/tmp/dircolors-solarized
 
-echo "Copy dircolors file to '~/.dircolors'..."
-cp tmp/src/dir_colors ~/.dircolors
+echo "Copy dircolors file to '~/.dircolors'"
+cp ~/tmp/dircolors-solarized/dircolors.256dark ~/.dircolors
 
 echo "Apply dircolors"
 eval `dircolors .dircolors`
 
 echo "Finish"
-
 
