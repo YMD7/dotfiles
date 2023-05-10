baab7d66601ce8292c9a128744df6ace320a4e22
Kyo Yamada <yamada.k@edocode.co.jp>
2023-05-10 23:27:01 +0900

    restructure

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,35 @@
+return {
+  -- Color schemes
+
+  {
+    -- kanagawa
+    "rebelot/kanagawa.nvim",
+    -- config = function()
+    --   require('kanagawa').setup({
+    --     colors = {
+    --       theme = { all = { ui = { bg_gutter = 'none' } } }
+    --     },
+    --   })
+    --   vim.cmd("colorscheme kanagawa-dragon")
+    -- end,
+  },
+
+  {
+    -- everforest
+    "sainnhe/everforest",
+    -- config = function()
+    --   vim.o.background = 'dark'
+    --   vim.g.everforest_background = 'hard'
+    --   vim.cmd("colorscheme everforest")
+    -- end,
+  },
+
+  {
+    -- alduin
+    "AlessandroYorba/Alduin",
+    config = function()
+      vim.g.alduin_Shout_Become_Ethereal = 1
+      vim.cmd("colorscheme alduin")
+    end,
+  },
+}
