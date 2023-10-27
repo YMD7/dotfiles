21f74968563170b930f7c06bf719f44497daf4ec
Kyo Yamada <yamada.k@edocode.co.jp>
2023-10-27 23:23:24 +0900

    add keymap

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -24,6 +24,12 @@ return {
       -- Disable virtual text
       vim.diagnostic.config({ virtual_text = false })
 
+      -- Keymap
+      vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+      vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+      vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+      vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
     end,
   },
 
